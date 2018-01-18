07e35bf95fad90634d90228f8191f46a894b7907
Unknown <satiros12@gmail.com>
2018-01-18 10:09:09 +0100

    APP ID and PASS

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 var connector = new botBuilder.ChatConnector({
-    appId: "",
-    appPassword: ""
+    appId: "5815f660-4db5-4571-9a4a-3f544934e149",
+    appPassword: "FNL]cysltpbMKPB02832];="
 });
 
 botFactory(connector);
